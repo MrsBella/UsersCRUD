49e3df25bba22d43aed31ec47f743b4107e038c3
Izabela Garbacz <izabelagarbacz@vp.pl>
2020-08-11 12:24:58 +0200

    usuwanie użytkownika

---
+++ src/main/java/pl/coderslab/users/UserDelete.java
@@ -0,0 +1,28 @@
+package pl.coderslab.users;
+
+import pl.coderslab.utils.User;
+import pl.coderslab.utils.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserDelete", value = "/users/delete")
+public class UserDelete extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+        UserDao userDao = new UserDao();
+
+        userDao.delete(Integer.parseInt(id));
+
+        response.sendRedirect("/users/list");
+    }
+}
